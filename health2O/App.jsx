885ac17cf1ddc69f50fade56e81b07e76a924449
Amy Huang <goten02028@gmail.com>
2024-02-13 21:27:21 -0800

    implemented signout function in home page

--- health2O/App.jsx
+++ health2O/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
         <Stack.Screen options={opt1} name="Login" component={LoginScreen} />
         <Stack.Screen options={opt1} name="Signup" component={SignupScreen} />
         <Stack.Screen name="NewUserInfo" component={NewUserInfoScreen} />
-        <Stack.Screen options={opt2} name="Home" component={HomeScreen} />
+        <Stack.Screen options={opt1} name="Home" component={HomeScreen} />
         <Stack.Screen name="Hydrate" component={HydrationScreen} />
         <Stack.Screen name="Sleep" component={SleepScreen} />
         <Stack.Screen name="Exercise" component={ExerciseScreen} />

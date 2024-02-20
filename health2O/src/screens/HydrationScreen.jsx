2a660a33419278327eaea22f9b139101cacb249f
Weiguang Wu <wwg12121@gmail.com>
2024-02-19 21:30:36 -0800

    Add function to enter target amount and select target unit

--- health2O/src/screens/HydrationScreen.jsx
+++ health2O/src/screens/HydrationScreen.jsx
@@ -1,5 +1,6 @@
-import React, {Component} from "react";
-import { View, Text, Image, StyleSheet, Pressable, Animated, TouchableOpacity} from "react-native";
+import React, {Component, useState} from "react";
+import { View, Text, Image, StyleSheet, Pressable, Animated, TouchableOpacity, TextInput, Keyboard, TouchableWithoutFeedback} from "react-native";
+import RNPickerSelect from 'react-native-picker-select';
 
 const profileImage = require("../../assets/health20.png");
 
@@ -7,7 +8,7 @@ class WaterBottle extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      waterLevel: new Animated.Value(0), // Represents the current water level
+      waterLevel: new Animated.Value(0), // current water level
     };
   }
 
@@ -17,9 +18,9 @@ class WaterBottle extends Component {
       Animated.timing(
         waterLevel,
         {
-          toValue: waterLevel._value + 0.2, // Fill the bottle completely
-          duration: 500, // Duration of the filling animation
-          useNativeDriver: false, // Important for certain animations
+          toValue: waterLevel._value + 0.125, // Fill water
+          duration: 500,
+          useNativeDriver: false,
         }
       ).start();
     }
@@ -44,6 +45,8 @@ class WaterBottle extends Component {
 }
 
 const HydrationScreen = ()  => {
+  const [amount, setAmount] = useState('');
+  const [selectedUnit, setSelectedUnit] = useState(null);
   const animated = new Animated.Value(1);
 
   const fadeIn = () => {
@@ -62,6 +65,7 @@ const HydrationScreen = ()  => {
   };
 
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
     <View style={styles.screen}>
 
       <View style={styles.header}>
@@ -76,16 +80,35 @@ const HydrationScreen = ()  => {
         </View>
       </View>
 
-      <View style={{flex: 1.5, flexDirection: "row"}}>
+      <View style={{flex: 1.2, flexDirection: "row"}}>
 
         <View style={styles.target}>
           <Text style={styles.subtitle}>Target</Text>
           <View style={{flexDirection: "row"}}>
             <View style={styles.targetbg}>
-              <Text style={styles.targettext}>Amount</Text>
+              <TextInput 
+                style={styles.targettext}
+                keyboardType="numeric" 
+                placeholder="Amount" 
+                value={amount} 
+                onChangeText={amount => setAmount(amount)}
+                onEndEditing={() => Keyboard.dismiss()}
+              />
             </View>
             <View style={styles.targetbg}>
-              <Text style={styles.targettext}>Unit</Text>
+              <RNPickerSelect
+                style={{inputIOS: {textAlign: "center"}}}
+                placeholder={{label: "Unit", value: null}}
+                value={selectedUnit}
+                onValueChange={(unit) => setSelectedUnit(unit)}
+                items={[
+                  { label: 'Grams', value: 'Grams' },
+                  { label: 'Miligrams', value: 'Miligrams' },
+                  { label: 'Ounces', value: 'Ounces' },
+                  { label: 'Liters', value: 'Liters' },
+                  { label: 'Mililiters', value: 'Mililiters' },
+                ]}
+              />
             </View>
           </View>
         </View>
@@ -105,6 +128,7 @@ const HydrationScreen = ()  => {
       </View>
 
     </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -134,7 +158,7 @@ const styles = StyleSheet.create({
   },
 
   rec: {
-    flex: 2,
+    flex: 1.5,
     backgroundColor: "lightpink",
     borderRadius: 10,
     marginLeft: 10,
@@ -150,7 +174,6 @@ const styles = StyleSheet.create({
   recbg: {
     backgroundColor: "white",
     borderRadius: 10,
-    textAlign: "left",
     marginLeft: 10,
     marginRight: 10
   },
@@ -171,14 +194,16 @@ const styles = StyleSheet.create({
   targetbg: {
     backgroundColor: "white",
     borderRadius: 10,
-    marginLeft: 10
+    marginLeft: 10,
+    marginRight: 10,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   targettext: {
     fontSize: 13,
-    padding: 10,
-    paddingLeft: 20,
-    paddingRight: 18
+    padding: 10
   },
 
   log: {
@@ -203,18 +228,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   bottle: {
-    width: 100,
-    height: 200,
-    borderColor: 'black',
-    borderWidth: 2,
+    width: 150,
+    height: 300,
+    borderColor: 'white',
+    borderWidth: 1,
     backgroundColor: 'transparent',
+    borderRadius: 20,
+    overflow: "hidden"
   },
+
   water: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'blue', // Change this to the color you want for water
-  },
+    backgroundColor: 'blue'
+  }
 });

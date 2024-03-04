544f16938e6b779c6dde4066bd51a7aca2972abd
Seraphine Wu <seraphine5summer@gmail.com>
2024-03-03 21:08:38 -0800

    Add functions to retrieve and update hydration data and display current amount/target

--- health2O/src/screens/HydrationScreen.jsx
+++ health2O/src/screens/HydrationScreen.jsx
@@ -1,6 +1,9 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { View, Text, Image, StyleSheet, Pressable, ScrollView, Animated, TouchableOpacity, TextInput, Keyboard, TouchableWithoutFeedback, SafeAreaView } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import { firebaseAuth, firebaseFirestore } from "../../FirebaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { Overlay } from "@rneui/base";
 
 const profileImage = require("../../assets/health20.png");
 
@@ -9,19 +12,33 @@ class WaterBottle extends Component {
     super(props);
     this.state = {
       waterLevel: new Animated.Value(0), // current water level
+      currentAmount: 0
     };
   }
 
   fillBottle = () => {
     const waterLevel = this.state.waterLevel;
-    if (waterLevel._value < 1.0) {
+    if (waterLevel._value < 1.0 && this.props.target && this.props.unit) {
       Animated.timing(waterLevel, {
         toValue: waterLevel._value + 0.125, // Fill water
         duration: 500,
         useNativeDriver: false,
-      }).start();
+      }).start(() => {
+        this.setState({ currentAmount: Math.round(waterLevel._value * this.props.target) });
+      });
     }
   };
+
+  targetReached = () => {
+    if (this.state.waterLevel._value >= 1) {
+      const userRef = doc(firebaseFirestore, "Users", firebaseAuth.currentUser.uid);
+      setDoc(userRef, 
+        {hydration: {date: Math.floor(Date.now() / 1000), amount: this.props.target, unit: this.props.unit}},
+        { merge: true }
+      );
+      return "Congrats, you have reached today's target!";
+    }
+  }
 
   render() {
     const waterLevel = this.state.waterLevel.interpolate({
@@ -29,11 +46,15 @@ class WaterBottle extends Component {
       outputRange: ["0%", "100%"],
     });
 
+    const {currentAmount} = this.state;
+
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={this.fillBottle}>
           <View style={styles.bottle}>
             <Animated.View style={[styles.water, { height: waterLevel }]} />
+            <Text style={styles.currentAmountText}>{currentAmount}/{this.props.target} {this.props.unit}</Text>
+            <Text style={[styles.currentAmountText, {top: "10%", color: "white"}]}>{this.targetReached()}</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -46,12 +67,55 @@ const HydrationScreen = () => {
   const [selectedUnit, setSelectedUnit] = useState(null);
   const animated = new Animated.Value(1);
 
+  const convertDate = (seconds) => {
+    const date = new Date(seconds * 1000);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Add 1 to month because it's zero-based
+    const day = ('0' + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  }
+  
+  const formatData = () => {
+    if (userData.hydration == undefined) {
+      return <Text>Loading...</Text>;
+    }
+    console.log(userData.hydration);
+    //return userData.hydration.map((item, index) => (<Text key={index}>{convertDate(item.date.seconds)}: {item.amount} {item.unit}{"\n"}</Text>));
+    return <Text>{convertDate(userData.hydration.date)}: {userData.hydration.amount} {userData.hydration.unit}</Text>;
+  };
+
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const currentUserId = firebaseAuth.currentUser.uid;
+        const document = await getDoc(doc(firebaseFirestore, "Users", currentUserId));
+        if (document.exists()) {
+          setUserData(document.data());
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
   const fadeIn = () => {
     Animated.timing(animated, {
       toValue: 0.4,
       duration: 100,
       useNativeDriver: true,
     }).start();
+    toggleOverlay();
   };
   const fadeOut = () => {
     Animated.timing(animated, {
@@ -100,8 +164,10 @@ const HydrationScreen = () => {
                     value={selectedUnit}
                     onValueChange={(unit) => setSelectedUnit(unit)}
                     items={[
-                      { label: "Grams", value: "Grams" },
-                      { label: "Miligrams", value: "Miligrams" },
+                      { label: "Cups", value: "Cups" },
+                      { label: "Pints", value: "Pints" },
+                      { label: "Quarts", value: "Quarts" },
+                      { label: "Gallons", value: "Gallons" },
                       { label: "Ounces", value: "Ounces" },
                       { label: "Liters", value: "Liters" },
                       { label: "Mililiters", value: "Mililiters" },
@@ -117,11 +183,15 @@ const HydrationScreen = () => {
                   <Text style={[styles.subtitle]}>Hydration Log</Text>
                 </Animated.View>
               </Pressable>
+              <Overlay isVisible={visible} onBackdropPress={toggleOverlay} 
+              overlayStyle={{backgroundColor: "white", width: 300, height: 500}}>
+                <Text>{formatData()}</Text>
+              </Overlay>
             </View>
           </View>
 
           <View style={styles.progressBar}>
-            <WaterBottle />
+            <WaterBottle target={amount} unit={selectedUnit}/>
           </View>
         </View>
       </TouchableWithoutFeedback>
@@ -244,5 +314,13 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     backgroundColor: "blue",
+  },
+
+  currentAmountText: {
+    position: 'absolute',
+    bottom: '50%',
+    alignSelf: 'center',
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
